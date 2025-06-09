a7a6456dfccae034759cfe3f4c0aea3052a0dc0d
MacBookAir <macbookair@mac-3.home>
2025-06-09 21:33:19 +0300

    Use real cat and dog images

--- api/images.js
+++ api/images.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.status(200).json({
-    image1: "https://via.placeholder.com/300x200?text=Image+1",
-    image2: "https://via.placeholder.com/300x200?text=Image+2",
+    image1: "https://my-api-project-sandy.vercel.app/cat.jpeg",
+    image2: "https://my-api-project-sandy.vercel.app/dog.jpeg",
   });
 }
