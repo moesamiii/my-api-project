29a73d0ba1d504d3c6aeda9e26cab780032ae283
MacBookAir <macbookair@mac-3.home>
2025-06-09 21:49:07 +0300

    Add CORS header

--- api/images.js
+++ api/images.js
@@ -1,4 +1,5 @@
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // 👈 add this line
   res.status(200).json({
     image1: "https://my-api-project-sandy.vercel.app/cat.jpeg",
     image2: "https://my-api-project-sandy.vercel.app/dog.jpeg",
